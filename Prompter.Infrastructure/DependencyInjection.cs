b54cfe4b26b98b241b135c83ad228467fd81d1c3
pwlpnv <pawelpnv@gmail.com>
2026-02-20 14:49:50 +0100

    Add DependencyInjection and Ollama

---
+++ Prompter.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Prompter.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection ConfigureIoC(this IServiceCollection services)
+    {
+        return services;
+    }
+}
