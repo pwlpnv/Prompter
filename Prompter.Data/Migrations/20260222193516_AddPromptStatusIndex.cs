cf48c7e7c3b0868a904044dfca5e02c5bd720c68
pwlpnv <pawelpnv@gmail.com>
2026-02-22 20:36:05 +0100

    new Status index added

---
+++ Prompter.Data/Migrations/20260222193516_AddPromptStatusIndex.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prompter.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPromptStatusIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Response",
+                table: "Prompts",
+                type: "character varying(10000)",
+                maxLength: 10000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prompts_Status",
+                table: "Prompts",
+                column: "Status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Prompts_Status",
+                table: "Prompts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Response",
+                table: "Prompts",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(10000)",
+                oldMaxLength: 10000,
+                oldNullable: true);
+        }
+    }
+}
