e3b6416c8afa3f64310650134a35cb878af927df
pwlpnv <pawelpnv@gmail.com>
2026-02-22 21:46:12 +0100

    rm todo

--- Prompter.Infrastructure/Llm/OllamaOptions.cs
+++ Prompter.Infrastructure/Llm/OllamaOptions.cs
@@ -6,7 +6,5 @@ public class OllamaOptions
 
     public string BaseUrl { get; set; } = "http://localhost:11434";
     public string Model { get; set; } = "phi3";
-
-    //TODO: remove or raise the limit
     public int MaxTokens { get; set; } = 40;
 }
